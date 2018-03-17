4f9e2e2f25f1c7f23c2a3dcb5927943ff3e2b159
David Lanier <dlanier@free.fr>
2018-03-17 16:13:18 +0100

    Add voice recognition with "go" and "stop" to switch on/off the relay

--- Relay/Relay/Relay.ino
+++ Relay/Relay/Relay.ino
@@ -15,10 +15,15 @@ defining CUSTOM_SETTINGS and the shields respective INCLUDE_ define.
 #define CUSTOM_SETTINGS
 #define INCLUDE_SMS_SHIELD
 #define INCLUDE_TERMINAL_SHIELD
+//Calling for Arduino Voice Recognize Shield
+#define INCLUDE_VOICE_RECOGNIZER_SHIELD
 
 /* Include 1Sheeld library. */
 #include <OneSheeld.h>
 
+//Set commands to be recognized by the Arduino Voice Recognition Shield
+const char on[] = "go";
+const char off[] = "stop";
 
 //relay is plugged in digital pin 7
 const int relayPin =7;
@@ -36,19 +41,37 @@ void setup()
 
 void loop() 
 {
+  //Check if 1Sheeld's Arduino Voice Recognition Shield received a new command
+  if(VoiceRecognition.isNewCommandReceived())
+  {
+    //Compare the last command received by the Arduino Voice Recognition Shield with the command "on"
+    if(!strcmp(on,VoiceRecognition.getLastCommand()))
+    {
+      //Then turn the relay on
+     digitalWrite(relayPin, HIGH); //Switch On
+    }
+    //Compare the last command received by the Arduino Voice Recognition Shield with the command "off"
+    else if(!strcmp(off,VoiceRecognition.getLastCommand()))
+    {
+      //Then turn the relay off
+     digitalWrite(relayPin, LOW); //Switch Off
+    }
+  }
 }
 
-/* This function will be invoked each time a new sms comes to smartphone. */
+/* This function will be invoked each time a new sms comes to the smartphone. 
+ *  You need to have the oneSheeld application installed on your smartphone and connectedto the Arduino shield
+*/
 void SMSReceivedFunction (String phoneNumber ,String messageBody)
 {
   Terminal.println(phoneNumber);
   Terminal.println(messageBody);
 
-  if (messageBody== String("On")){
+  if (messageBody.equalsIgnoreCase(String("on"))){
       digitalWrite(relayPin, HIGH); //Switch On
   }
   else
-  if (messageBody== String("Off")){
+  if (messageBody.equalsIgnoreCase(String("off"))){
       digitalWrite(relayPin, LOW); //Switch Off
   }
 }
